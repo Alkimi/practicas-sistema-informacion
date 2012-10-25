3155026e9f1bef3b3b6f5ff5c850ff0d7d30cc4d
enmica04@alumnos.unex.es <enmica04@alumnos.unex.es@f5807d0a-c1b4-cff5-e8e2-faf2a41e05c1>
2012-10-25 00:29:30 +0000

    primera prueba de concepto. ya se puede enviar informacion del applet a servlet y viceversa y realizar las consultas sql.
    la forma de comunicacion es crear un ResultSet simplificado que solo son los valores en una cadena de texto larga enviada de un lado a otro
    
    la prueba es funcional y se puede ampliar para nuestros propositos sin problemas.

---
+++ AplicacionWeb/src/java/ConexionBD.java
@@ -0,0 +1,190 @@
+
+
+
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ConexionBD {
+    private String userName;
+    private String password;
+    private String baseDatos;
+    private String servidor;
+    private Connection conexion=null;
+    public Statement instruccion=null;
+    ResultSet conjuntoResultados=null; // resultado de la ultima consulta
+
+    /**
+     * Constuctor parametrizado para conectarse a una base de dato en el servidor
+     * local es decir LOCALHOST, se le pasa por parametro el usuario, la contraseña
+     * y el nombre de la base de datos, puede lanzar excepciones
+     * 
+     * @param Usuario, nombre del usuario con acceso a la base de datos
+     * @param Password, contraseña del usuario con acceso a la base de datos
+     * @param BaseDatos, nombre de la base de datos,
+     * @throws ClassNotFoundException, lanza esta excepcion si no esta el conector jdbc
+     * @throws SQLException, lanza esta excepcion si no conecta con la base de datos
+     */
+    public ConexionBD(String Usuario, String Password, String BaseDatos) throws ClassNotFoundException, SQLException {
+        userName=Usuario;
+        password=Password;
+        baseDatos=BaseDatos;
+        servidor="localhost";
+        inicializa();
+   }
+
+    /**
+     * Constuctor parametrizado para conectarse a una base de dato en el servidor
+     * local es decir LOCALHOST, se le pasa por parametro el usuario, la contraseña
+     * la url donde esta el servidor y el nombre de la base de datos, puede lanzar excepciones
+     * 
+     * @param Usuario, nombre del usuario con acceso a la base de datos
+     * @param Password, contraseña del usuario con acceso a la base de datos
+     * @param BaseDatos, nombre de la base de datos,
+     * @param url, direcion url de donde esta la base de datos
+     * @throws ClassNotFoundException, lanza esta excepcion si no esta el conector jdbc
+     * @throws SQLException, lanza esta excepcion si no conecta con la base de datos
+     */
+   public ConexionBD(String Usuario, String Password, String BaseDatos, String url) throws ClassNotFoundException, SQLException{
+        userName=Usuario;
+        password=Password;
+        baseDatos=BaseDatos;
+        servidor=url;
+        inicializa();
+   }
+   
+   /**
+    * Crea la conexion a la base de datos con los parametros adecuados internos del obejeto.
+    * 
+     * @throws ClassNotFoundException, lanza esta excepcion si no esta el conector jdbc
+     * @throws SQLException, lanza esta excepcion si no conecta con la base de datos
+    */
+   private void inicializa() throws ClassNotFoundException, SQLException{
+        Class.forName("com.mysql.jdbc.Driver"); 
+        conexion = DriverManager.getConnection("jdbc:mysql://"+servidor+"/"+baseDatos, userName , password);
+        instruccion = conexion.createStatement();
+   }
+   
+   /**
+    * Devuelve una consulta del tipo SELECT formateada  o una excepcion si la 
+    * consulta es invalida
+    * 
+    * @param Cadena, cadena ha de contener el selec de la consulta SQL
+    * 
+    * @return la consulta formateada
+    * @throws SQLException, se produce la excepcion si la consulta no es correcta
+    */
+   public StringBuilder consultaYmostrar(String Cadena) throws SQLException{
+       StringBuilder resultados = new StringBuilder();
+       try{
+          conjuntoResultados = instruccion.executeQuery (Cadena);
+
+
+          ResultSetMetaData metaDatos = conjuntoResultados.getMetaData();
+          int numeroDeColumnas = metaDatos.getColumnCount();
+          for (int i = 1; i <= numeroDeColumnas; i++) {
+             resultados.append(metaDatos.getColumnName(i) + "\t");
+          }
+          resultados.append("\n");
+          while (conjuntoResultados.next()) {
+            for (int i = 1; i <= numeroDeColumnas; i++) {
+                resultados.append(conjuntoResultados.getObject(i) + "\t");
+            }
+            resultados.append("\n");
+          }
+       } catch (SQLException ex) {
+          throw ex;
+       }
+       return resultados;
+   }
+   
+   /**
+    * Realiza una consulta del tipo SELECT a la base de datos, lanza una expcepcion
+    * si la consulta es invalida
+    * 
+    * @param Cadena, la cadena que contiene la consulta en SQL
+    * 
+    * @return la consulta realizda en la base de datos en un resulSet
+    * @throws SQLException, se produce la excepcion si la consulta no es correcta
+    */
+   public ResultSet consultaSelect (String Cadena) throws SQLException{
+        try {
+            conjuntoResultados = instruccion.executeQuery (Cadena);
+        } catch (SQLException ex) {
+            throw ex;
+        }
+        return conjuntoResultados;
+   }
+
+   /**
+    * Realiza una consulta a la base de dato del tipo:
+    * DELETE, INSERT, CREATE TABLE, UPDATE, DROP, lanza un expcecion si
+    * la consulta es invalida
+    * 
+    * @param Cadena, la cadena que contiene la consulta en SQL
+    * 
+    * @return o bien el numero de filas  para declaraciones SQL Data Manipulation Language (DML) 
+    * y 0  para las sentencias de SQL que no devuelven nada
+    * 
+    * @throws SQLException, se produce la excepcion si la consulta no es correcta
+    */
+   public boolean consultaUpdate(String Cadena) throws SQLException{
+       boolean aux;
+        try {
+            aux= instruccion.execute(Cadena);
+        } catch (SQLException ex) {
+            throw ex;
+        }
+        return aux;
+     }
+   
+   /**
+    * Devuelve el ResulSet de la ultima consulta realizad a la base de datos
+    * 
+    * @return El ultimo ResulSet de la consulta SQL  valida ejecutada
+    * 
+    */
+   public ResultSet ultimaConsulta(){
+       return conjuntoResultados;
+   }
+   
+   
+   /**
+    * Cierra la conexion a la base de datos
+    */
+   public void cerrarBaseDatos(){
+        try {
+            conexion.close();
+        } catch (SQLException ex) {
+           
+        }
+   }
+   
+   /**
+    * Devuelve el numero total de filas de la ultima 
+    * consulta valida realizada en SQL
+    * 
+    * @return El numero de filas
+    */
+       public int totalFilas() {
+        int numFilas = 0;
+        try {
+
+            conjuntoResultados.last();
+            numFilas = conjuntoResultados.getRow();
+            conjuntoResultados.beforeFirst();
+
+        } catch (Exception e) {
+        }
+        return numFilas;
+    }
+
+}
--- AplicacionWeb/src/java/Practica1.java
+++ AplicacionWeb/src/java/Practica1.java
@@ -4,7 +4,31 @@
  */
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
+import java.io.Reader;
+import java.math.BigDecimal;
+import java.net.URL;
+import java.sql.Array;
+import java.sql.Blob;
+import java.sql.Clob;
+import java.sql.Date;
+import java.sql.NClob;
+import java.sql.Ref;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.RowId;
+import java.sql.SQLException;
+import java.sql.SQLWarning;
+import java.sql.SQLXML;
+import java.sql.Statement;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,6 +40,33 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class Practica1 extends HttpServlet {
 
+     ConexionBD conexion = null;
+     
+     /**
+      * Funcion principal que se inica cuando se incia el servidor
+      * @param config contiene la configuracion local del servidor
+      */
+    @Override
+     public void init (ServletConfig config){
+        try {
+            conexion = new  ConexionBD("root","toor","consumoelectrico");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Practica1.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(Practica1.class.getName()).log(Level.SEVERE, null, ex);
+        }
+     }
+     
+     /**
+      * Destructor del servidor
+      */
+    @Override
+     public void destroy(){
+         conexion.cerrarBaseDatos();
+     }
+    
+ 
+    
     /**
      * Processes requests for both HTTP
      * <code>GET</code> and
@@ -28,21 +79,28 @@ public class Practica1 extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        String cadena =null;
+        
+        
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
+         try{
+            if (Boolean.parseBoolean(request.getParameter("PeticionInicio"))){
+
+                cadena = consultaProvincias();
+            //llamamos a la bd para obtener las provincias y enviarlas
+            }
+          } catch(Exception e){
+            out.println("Error en cálculo de factorial. "+e.getMessage());
+        }  
+                
         try {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Practica1</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Practica1 at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        } finally {            
+             out.println(cadena);
+         } finally {            
             out.close();
         }
+        
+        
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -85,4 +143,33 @@ public class Practica1 extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
+    
+    private String consultaProvincias(){
+        String cadena="";
+        ResultSet consulta;    
+        int columnas;
+        try {
+
+            consulta=  conexion.consultaSelect("Select * from provincias");
+
+
+        columnas=consulta.getMetaData().getColumnCount();
+        while(consulta.next()){
+            for (int i=1;i<=columnas;i++){
+                cadena=cadena+consulta.getObject(i).toString();
+                if (i==columnas){
+                    cadena=cadena+"\n";
+                    
+                }
+                else {
+                    cadena=cadena+";";
+                }
+            }
+        }
+        } catch (SQLException ex) {
+           // Logger.getLogger(Practica1.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+        return cadena;
+    }
 }
